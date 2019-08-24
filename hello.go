ac2f300badb3a2353445cdcf00383b92aec38fe4
danielVg <danielveragi@gmail.com>
2019-08-24 15:41:22 +0200

    Code Refactoring -> External function to get language

--- hello.go
+++ hello.go
@@ -4,18 +4,35 @@ import (
 	"fmt"
 )
 
-var englishHelloPrefix = "Hello "
+const spanish = "Spanish"
+const german = "Deutsch"
+const englishHelloPrefix = "Hello "
+const spanishHelloPrefix = "Hola "
+const GermanHelloPrefix = "Hallo "
 
 // Hello returns "Hello World"
-func Hello(name string) string {
+func Hello(name string, language string) string {
 
 	if name == "" {
 		name = "World"
 	}
 
-	return englishHelloPrefix + name
+	return greetingPrexif(language) + name
+}
+
+func greetingPrexif(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case german:
+		prefix = GermanHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+
+	return
 }
 
 func main() {
-	fmt.Println(Hello("Daniel"))
+	fmt.Println(Hello("Daniel", ""))
 }
