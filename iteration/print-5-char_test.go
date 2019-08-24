45a7f64f8cad1ae82d1986b6147cab554912d326
danielVg <danielveragi@gmail.com>
2019-08-24 16:05:30 +0200

    Test to print 5 chars

---
+++ iteration/print-5-char_test.go
@@ -0,0 +1,12 @@
+package iteration
+
+import "testing"
+
+func TestRepeat(t *testing.T) {
+	repeated := Repeat("a")
+	expected := "aaaaa"
+
+	if repeated != expected {
+		t.Errorf("Got %q, want %q", repeated, expected)
+	}
+}
