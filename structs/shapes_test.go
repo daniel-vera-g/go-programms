873579da769832b244036d813caeac696e29b96e
danielVg <danielveragi@gmail.com>
2019-08-27 18:52:26 +0200

    More explicit renaming of has are

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -7,29 +7,29 @@ func TestPerimeter(t *testing.T) {
 	rectangle := Rectangle{10.0, 20.0}
 
 	got := Perimeter(rectangle)
-	want := 400.0
+	hasArea := 400.0
 
-	if got != want {
-		t.Errorf("Got %.2f, want %.2f", got, want)
+	if got != hasArea {
+		t.Errorf("Got %.2f, hasArea %.2f", got, hasArea)
 	}
 }
 
 func TestArea(t *testing.T) {
 
 	areTests := []struct {
-		shape Shape
-		want  float64
+		shape   Shape
+		hasArea float64
 	}{
-		{shape: Rectangle{Width: 10.0, Length: 20.0}, want: 200.0},
-		{shape: Circle{Radius: 10}, want: 314.1592653589793},
-		{shape: Triangle{Base: 12, Height: 6}, want: 36.0},
+		{shape: Rectangle{Width: 10.0, Length: 20.0}, hasArea: 200.0},
+		{shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
 	}
 
 	for _, tt := range areTests {
 		got := tt.shape.Area()
 
-		if got != tt.want {
-			t.Errorf("Got %.2f, wanr %.2f", got, tt.want)
+		if got != tt.hasArea {
+			t.Errorf("Got %.2f, wanr %.2f", got, tt.hasArea)
 		}
 	}
 }
